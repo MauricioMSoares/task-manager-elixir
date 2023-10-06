8e51114b497f01e6e07f35ce8484eee47f0b6feb
MauricioMSoares <mauricio.soares@sptech.school>
2023-10-06 16:44:43 -0300

    Initial config

---
+++ lib/task_manager.ex
@@ -0,0 +1,18 @@
+defmodule TaskManager do
+  @moduledoc """
+  Documentation for `TaskManager`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TaskManager.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
